5b0e8146acfd4c882ca4c577cb35d3b66aae6814
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-06-06 20:59:34 +0700

    first commit

---
+++ app/src/main/java/com/example/mvcstructure/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mvcstructure.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mvcstructure.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
